20f78af8f9d26908d8a95556f59b3f52cfffcc30
Oli Zimpasser <oglimmer@gmail.com>
2018-08-12 19:30:39 +0200

    Adding Fulgensfile.js

---
+++ Fulgensfile.js
@@ -0,0 +1,32 @@
+module.exports = {
+
+  config: {
+    Name: "GridGameOne",
+    Vagrant: {
+      Box: 'ubuntu/xenial64',
+      Install: 'maven openjdk-8-jdk-headless npm phantomjs docker.io',
+      AddInstall: [
+        'ln -s /usr/bin/nodejs /usr/bin/node',
+        'npm install -g jasmine phantomjs-prebuilt',
+      ]
+    },
+  },
+
+  software: {
+    "ggo": {
+      Source: "mvn",
+      Mvn: {
+        BuildDependencies: {
+          apt: [ "npm", "phantomjs" ],
+          npm: [ "jasmine", "phantomjs-prebuilt" ]
+        }
+      },
+      Artifact: "web/target/grid.war"
+    },
+
+    "tomcat": {
+      Source: "tomcat",
+      Connect: "ggo"
+    }
+  }
+}
