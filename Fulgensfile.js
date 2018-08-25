118b0600588b157be745d3a08cde7341cb3beec6
Oli Zimpasser <oglimmer@gmail.com>
2018-08-25 17:23:52 +0200

    Remove dependency to phantomjs as it will be automatically downloaded via npm

--- Fulgensfile.js
+++ Fulgensfile.js
@@ -4,10 +4,10 @@ module.exports = {
     Name: "GridGameOne",
     Vagrant: {
       Box: 'ubuntu/xenial64',
-      Install: 'maven openjdk-8-jdk-headless npm phantomjs docker.io',
+      Install: 'maven openjdk-8-jdk-headless npm docker.io',
       AddInstall: [
         'ln -s /usr/bin/nodejs /usr/bin/node',
-        'npm install -g jasmine phantomjs-prebuilt',
+        'npm install -g jasmine',
       ]
     },
   },
@@ -17,8 +17,8 @@ module.exports = {
       Source: "mvn",
       Mvn: {
         BuildDependencies: {
-          apt: [ "npm", "phantomjs" ],
-          npm: [ "jasmine", "phantomjs-prebuilt" ]
+          apt: [ "npm" ],
+          npm: [ "jasmine" ]
         }
       },
       Artifact: "web/target/grid.war"
